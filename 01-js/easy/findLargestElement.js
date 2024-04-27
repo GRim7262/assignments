2b5c41a4e55e8ad611c91dac758a1fe92a15ca42
Vinal Pawar <vinalpawar1@gmail.com>
2024-04-27 22:22:44 +0530

    solved easy to medium

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -4,9 +4,17 @@
   - Input: [3, 7, 2, 9, 1]
   - Output: 9
 */
-
+const numbers = [3, 7, 2, 9, 1];
 function findLargestElement(numbers) {
-    
+  let largeNum = numbers[0];
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > largeNum) {
+      largeNum = numbers[i];
+    }
+  }
+  return largeNum;
 }
+console.log(findLargestElement(numbers));
 
 module.exports = findLargestElement;
