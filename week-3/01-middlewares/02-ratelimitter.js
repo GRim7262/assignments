51ee871a18ce9e8e4466d902a076cac0cf2df507
Vinal Pawar <vinalpawar1@gmail.com>
2026-05-04 21:37:47 +0530

    week-3 middlwares done

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -13,14 +13,37 @@ const app = express();
 
 let numberOfRequestsForUser = {};
 setInterval(() => {
-    numberOfRequestsForUser = {};
+  numberOfRequestsForUser = {};
 }, 1000)
 
-app.get('/user', function(req, res) {
+const rateLimitter = (req, res, next) => {
+  const userId = req.headers['user-id'];
+
+  if (!userId) {
+    return res.status(404).send("User ID missing")
+  }
+
+  if (!numberOfRequestsForUser[userId]) {
+    numberOfRequestsForUser[userId] = 1;
+
+  } else {
+    numberOfRequestsForUser[userId]++;
+  }
+
+  if (numberOfRequestsForUser[userId] > 5) {
+    return res.status(404).send("Too many requests")
+  }
+
+  next()
+}
+
+app.use(rateLimitter)
+
+app.get('/user', function (req, res) {
   res.status(200).json({ name: 'john' });
 });
 
-app.post('/user', function(req, res) {
+app.post('/user', function (req, res) {
   res.status(200).json({ msg: 'created dummy user' });
 });
 
